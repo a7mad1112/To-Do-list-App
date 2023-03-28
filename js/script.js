d00f3f09ae4d6e32c8a2c31d371d08a0cbd00308
a7mad1112 <ahmalawneh79@gmail.com>
2023-03-28 21:30:07 +0300

    handle delete completed tasks

--- js/script.js
+++ js/script.js
@@ -55,8 +55,8 @@ function getTasks() {
       },
     ]
   };
-  localStorage.setItem("tasks", JSON.stringify(tasks));
   */
+  localStorage.setItem("tasks", JSON.stringify(tasks));
   tasks.completed.forEach((task, i) => {
     template += `
     <div class="content-box">
@@ -80,7 +80,7 @@ function getTasks() {
       <p id="end-date" class="mt-1">Date: ${task.date}</p>
       <!-- <p class="priority">Priority: High</p> -->
       <p class="is-done text-end">
-        <input type="checkbox" name="isComplete" onclick=CompleteTask(${i})>
+        <input type="checkbox" name="isComplete" onclick=UnCompleteTask(${i})>
       </p>
     </div>
   </div>
@@ -100,7 +100,7 @@ function getTasks() {
         <span id="edit-task-btn">
           <i class="fa-solid fa-pen"></i>
         </span>
-        <span id="delete-task-btn" role="button" aria-label="Delete task">
+        <span id="delete-task-btn" role="button" aria-label="Delete task" onclick=deleteTheCompleted(${i})>
           <i class="fa-solid fa-trash"></i>
         </span>
       </div>
@@ -112,7 +112,7 @@ function getTasks() {
       <p id="end-date" class="mt-1">Date: ${task.date}</p>
       <!-- <p class="priority">Priority: High</p> -->
       <p class="is-done text-end">
-        <input type="checkbox" name="isComplete" checked onclick=UnCompleteTask(${i})>
+        <input type="checkbox" name="isComplete" checked onclick=CompleteTask(${i}) >
       </p>
     </div>
   </div>
@@ -127,15 +127,15 @@ function getTasks() {
 getTasks();
 /* Handle complete and unComplete task checkbox */
 function CompleteTask(index) {
-  if (index < 0 || index >= tasks.completed.length) return;
-  tasks.unCompleted.push(tasks.completed.splice(index, 1)[0]);
+  if (index < 0 || index >= tasks.unCompleted.length) return;
+  tasks.completed.push(tasks.unCompleted.splice(index, 1)[0]);
   localStorage.setItem("tasks", JSON.stringify(tasks));
   getTasks();
 }
 
 function UnCompleteTask(index) {
-  if (index < 0 || index >= tasks.unCompleted.length) return;
-  tasks.completed.push(tasks.unCompleted.splice(index, 1)[0]);
+  if (index < 0 || index >= tasks.completed.length) return;
+  tasks.unCompleted.push(tasks.completed.splice(index, 1)[0]);
   localStorage.setItem("tasks", JSON.stringify(tasks));
   getTasks();
 }
@@ -152,9 +152,66 @@ function accordionToggle() {
 }
 /* handle show and close adding new task form*/
 document.getElementsByClassName("add-task")[0].onclick = () => {
-  document.getElementById("add-task-form").style.display = "flex";
+  document.getElementById("add-task-form").style.transform = "scale(1)";
 };
 document.querySelector(".close-add-task-form").onclick = () => {
-  document.getElementById("add-task-form").style.display = "none";
+  document.getElementById("add-task-form").style.transform = "scale(0)";
 };
-
+/* input validation for add new task */
+let validate = false;
+document.getElementById("task-name").addEventListener("input", (e) => {
+  document.querySelector(".title-err").innerHTML = "";
+  validate = true;
+  if (e.target.value.trim().length == 0) {
+    document.querySelector(".title-err").innerHTML =
+      "Task must include a title.";
+    validate = false;
+  }
+});
+/* handle submit form */
+document.getElementById("add-task-modal").addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!validate) return;
+  let title = document.getElementById("task-name").value;
+  let details = document.getElementById("task-details").value;
+  let priority = document.getElementById("select-priority").value;
+  let date = document.getElementById("due-date").value;
+  let newTask = {
+    title,
+    priority,
+    details,
+    date,
+  };
+  // console.log(newTask);
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || {};
+  tasks.completed.push(newTask);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  /* empty and close the form */
+  let inputs = [
+    ...document.querySelectorAll(
+      "#add-task-modal input, #add-task-modal textarea, #add-task-modal select"
+    ),
+  ];
+  inputs.forEach((e) => (e.value = ""));
+  getTasks();
+  document.getElementById("add-task-form").style.display = "none";
+});
+/* change the color of dot after priority in the add form */
+document
+  .querySelector(".my-modal-content select")
+  .addEventListener("input", function (e) {
+    document
+      .querySelector('label[for="select-priority"]')
+      .setAttribute("data-color", e.target.value);
+  });
+/* handle delete task button */
+function deleteTheCompleted(index) {
+  // console.l  og(tasks);
+  tasks = {
+    completed: tasks.completed,
+    unCompleted: tasks.unCompleted.filter((e, i) => +i !== +index),
+  };
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  getTasks();
+}
+deleteTheCompleted();
